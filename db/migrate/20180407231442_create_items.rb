1f63a47af701a82488af0a98857fd9b6ae8f9f40
Galiant <antonijodev@gmail.com>
2018-04-07 23:27:10 +0000

    Generate scaffold Item

---
+++ db/migrate/20180407231442_create_items.rb
@@ -0,0 +1,15 @@
+class CreateItems < ActiveRecord::Migration[5.1]
+  def change
+    create_table :items do |t|
+      t.string :title
+      t.string :console
+      t.string :brand
+      t.string :genre
+      t.text :description
+      t.decimal :price
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
