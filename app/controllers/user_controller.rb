a99e0e77dc8cf4616da97f8bf23b2d119dc90850
isabelle <isa.bidou@gmail.com>
2018-04-12 11:10:49 +0000

    isabelle added Cart as per video of 10-03

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,15 @@
+class UserController < ApplicationController
+    
+    def login
+        session[:login] = 1
+        flash[:notice] = "you are logged in"
+        redirect_to :controller => :items
+    end
+    def logout
+        #in case things go crazy and impossible to understand uncomment the line below and logout /login 
+        #session[:cart] = nil
+        session[:login] = nil
+        flash[:notice] = "Goodbye!"
+        redirect_to :controller => :items
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  get 'cart/index'
+ 
+
   resources :items
   root 'static_pages#home'
 
@@ -8,6 +11,14 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
 
   get '/help', to: 'static_pages#help'
-
+  
+  get '/login', to: 'user#login'
+  get '/logout', to: 'user#logout'
+  
+   get '/cart', to: 'cart#index'
+   get '/cart/:id', to: 'cart#add'
+   get '/cart/remove/:id', to: 'cart#remove'
+  
+   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
