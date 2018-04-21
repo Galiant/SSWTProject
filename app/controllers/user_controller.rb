ae59b0f0657ed2510d36cc8d0dff7c5936c15f7b
isabelle <isa.bidou@gmail.com>
2018-04-21 11:35:24 +0000

    isabelle implemented admin user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,6 +5,8 @@ class UserController < ApplicationController
         flash[:notice] = "you are logged in"
         redirect_to :controller => :items
         #session[:cart] = nil
+        
+        current_user.update_attribute :admin, true
     end
     def logout
         #in case things go crazy and impossible to understand uncomment the line below and logout /login 
